30eed2ca0f94267dfd4167cec8721d8843a4f432
Austin Oh <austinoh@kindartnews-lm.corp.yahoo.com>
2015-01-16 15:47:03 -0800

    Initial Commit

---
+++ tipcalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tipcalculator
+//
+//  Created by Austin Oh on 1/16/15.
+//  Copyright (c) 2015 Austin Oh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
