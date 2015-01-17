ef0c136508633f9bf2ebf6876809d0e67692bb0a
Austin Oh <austinoh@kindartnews-lm.corp.yahoo.com>
2015-01-16 17:40:01 -0800

    first commit

---
+++ tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Austin Oh on 1/16/15.
+//  Copyright (c) 2015 Austin Oh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
